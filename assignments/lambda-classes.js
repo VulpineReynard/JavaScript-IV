87bceb847c1d6ce441bb5ddf906acc65decae10b
VulpineReynard <milare8@gmail.com>
2019-06-13 15:00:00 -0400

    Created Student class and set up its inheritance to Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -5,6 +5,7 @@ class Person {
     this.age = attrs.age;
     this.location = attrs.location;
   }
+  // PERSON FUNCTIONS
   speak(){
     console.log(`Hello my name is ` + this.name + `, I am from ` + this.location);
   }
@@ -12,6 +13,35 @@ class Person {
 
 class Instructor extends Person {
   constructor(instructorAttrs) {
-    super(instructorAttrs)
+    super(instructorAttrs);
+    this.specialty = instructorAttrs.specialty;
+    this.favLanguage = instructorAttrs.favLanguage;
+    this.catchPhrase = instructorAttrs.catchPhrase;
+  }
+  // INSTRUCTOR FUNCTIONS
+  demo(subject) {
+    console.log("Today we are learning about " + subject);
+  }
+  grade(student, subject) {
+    console.log(student + " receives a perfect score on " + subject);
+  }
+}
+
+class Student extends Person {
+  constructor(studentAttrs){
+    super(studentAttrs);
+    this.previousBackground = studentAttrs.previousBackground;
+    this.className = studentAttrs.className;
+    this.favSubjects = studentAttrs.favLanguage;
+  }
+  // STUDENT FUNCTIONS
+  listsSubjects(){
+    this.favSubjects.forEach((element) => { console.log(element); });
+  }
+  PRAssignment(subject) {
+    console.log(this.name + " has submitted a PR for " + subject);
+  }
+  sprintChallenge(subject) {
+    console.log(this.name + " has begun the sprint challenge on " + subject);
   }
 }

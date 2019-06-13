2659d946ff5bce01782a9b52faf6a1bbd98af476
VulpineReynard <milare8@gmail.com>
2019-06-13 14:45:44 -0400

    Created Person and Instructor objects. Set up inheritance to Insctructor from Person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,17 @@
 // CODE here for your Lambda Classes
+class Person {
+  constructor(attrs) {
+    this.name = attrs.name;
+    this.age = attrs.age;
+    this.location = attrs.location;
+  }
+  speak(){
+    console.log(`Hello my name is ` + this.name + `, I am from ` + this.location);
+  }
+}
+
+class Instructor extends Person {
+  constructor(instructorAttrs) {
+    super(instructorAttrs)
+  }
+}
